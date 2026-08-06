38d5a2711478e9f056bbfb2f29f549680b6e3cb8
Henry <hgriffy@student.cscc.edu>
2026-08-05 20:02:31 -0400

    M3: Update docstrings for ship

--- ship.py
+++ ship.py
@@ -1,41 +1,29 @@
-"""Class to define protagonist ship + actions
-Depends on:
-* settings.py
-* 
+"""Manages the ship that the player, aka, playable character in the game."""
 
-Is Depended on:
-* alien_invasion.py
-* 
-
-Properties contain:
-* 
-
-Methods control:
-* 
-"""
-
+# Python modules
 from typing import TYPE_CHECKING
+# Installed modules
 import pygame
-
+# Custom/game modules
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
     from arsenal import Arsenal
 
 class Ship():
-    """Sprite to represent the ship/playable character"""
+    """Sprite to represent the ship/playable character."""
 
     def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal') -> None:
         self.game: AlienInvasion = game
         self.settings = game.settings
         self.screen = game.screen
-        self.boundaries = self.screen.get_rect()
+        self.boundaries: pygame.Rect = self.screen.get_rect()
 
         self.image = pygame.image.load(self.settings.ship_file)
         self.image = pygame.transform.scale(self.image,
             (self.settings.ship_w,self.settings.ship_h)
             )
 
-        self.rect = self.image.get_rect()
+        self.rect: pygame.Rect = self.image.get_rect()
         self.x: int
         self._center_ship()
 
@@ -45,18 +33,22 @@ class Ship():
         self.arsenal: Arsenal = arsenal
 
     def _center_ship(self) -> None:
-        """return ship to the middle of the screen"""
+        """Returns ship to the middle of the screen.
+        
+        Effects:
+            Changes ship location variable to default.
+        """
         self.rect.midbottom = self.boundaries.midbottom
         self.x = self.rect.x
 
     def update(self) -> None:
-        """update the position of the ship"""
+        """Updates + represents the ship + triggers update of lasers/cannonballs."""
         self._update_ship_movement()
         self.arsenal.update_arsenals()
 
     def _update_ship_movement(self) -> None:
-        """calculate position of the ship (for self.update())"""
-        temp_speed: int = self.settings.ship_speed
+        """Calculates position of the ship (for self.update())"""
+        temp_speed: int = self.settings.ship_speed # holder to simplify lines below
         if self.moving_left and self.rect.left > self.boundaries.left:
             self.x -= temp_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
@@ -64,17 +56,26 @@ class Ship():
         self.rect.x = self.x
 
     def draw(self) -> None:
-        """implement the representation of the ship onto the screen"""
+        """implements the representation of the ship onto the screen."""
         self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
 
     def fire(self, ammo_type) -> bool:
-        """trigger the code to fire a bullet"""
+        """Triggers the code to fire a bullet.
+        
+        Returns:
+            [bool]: Indicates whether laser/cannonball was fired.
+        """
         return self.arsenal.fire_bullet(ammo_type)
 
     def check_collisions(self, other_group) -> bool:
-        """determine if the ship has collided with another screen element (ie alien)"""
+        """Determines if the ship has collided with another screen element (ie alien).
+        
+        Returns:
+            [bool]: True if there was a collision, False otherwise.
+        """
         if pygame.sprite.spritecollideany(self, other_group):
             self._center_ship()
             return True
         return False
+    
